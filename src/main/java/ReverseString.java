ffa14d6f724d16725c9e3c1255ec8f632602b2cb
TAKAHIRO-03 <takachanman1703@gmail.com>
2022-12-13 18:15:59 +1100

    Reverse String

---
+++ src/main/java/ReverseString.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+        var s = new char[]{'h', 'e', 'l', 'l', 'o'};
+
+        for (int i = 0, len = s.length / 2; i < len; i++) {
+            var tmp1 = s[i];
+            var tmp2 = s[s.length - 1 - i];
+            s[i] = tmp2;
+            s[s.length - 1 - i] = tmp1;
+        }
+
+        System.out.println(Arrays.toString(s));
+
+    }
+
+}
